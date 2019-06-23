30d44f7015928dc6ed4eeba6b06be6b19ec96a5b
praktischer_teil <48922249+PraktischerTeil@users.noreply.github.com>
2019-06-23 20:27:25 +0200

    Update

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ $(function() {
 	// If the study is called with a parameter for redirection, as explained in the documentation, this value is overwritten.
 	// To the redirect link, the following information will be appended: (1) participant number, (2) condition, (3) username, (4) description submitted by participant. These variables can be extracted from the link, saved as data, and used for linking the Social Media Ostracism paradigm to subsequent tasks and measures. See documentation for more details.
 
-    settings.defaultredirect = 'http://ww2.unipark.de/uc/unimannheim';
+    settings.defaultredirect = 'https://www.soscisurvey.de/masterarbeit_soziale_netzwerke2/';
 
 
 	// **Tasklength**
@@ -450,7 +450,7 @@ function DeaktiviereLike(){
 
 	var urlHasQuestionMark = (window.redirect.indexOf("?") > -1);
 	if(!urlHasQuestionMark) {
-		window.redirect = window.redirect+"?redir=1";
+		window.redirect = window.redirect+"?";
 	}
 	//alert(window.redirect);
 
